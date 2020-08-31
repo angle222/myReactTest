3d055bb4dedb469c152bce07a5568133250d0543
angle222 <460690092@qq.com>
2020-08-31 23:18:22 +0800

    自定义高阶组件的form

--- src/components/MyForm.js
+++ src/components/MyForm.js
@@ -5,21 +5,69 @@ import { Input, Button } from 'antd';
 // 创建高阶组件
 function MyFormCreate(Comp) {
     return class extends Component {
-        // 字段校验
-        validateFile() {
+        constructor(props){
+            super(props)
+            this.state = {}
+            this.options = {}
+            // {name:{rules:[{required:true}]}}
+        }
+        validateFiles=()=> {
             console.log('我是校验')
         }
+        // 字段校验
+        validateFile=(name)=> {
+            console.log('校验',name)
+            const rules = this.options[name].rules;//得到是一个数组
+            const result = rules.some(rule=>{
+                if(rule.required){
+                    if(!this.state[name]){
+                        // 必填项为false
+                        this.setState({
+                            // 错误信息设置              
+                            [name + "Message"]: rule.message
+                        })
+                        return true
+                    }
+                }
+            })
+            console.log(result)
+            if(!result){
+                this.setState({
+                    // 错误信息设置              
+                    [name + "Message"]: ''
+                })
+            }
+        }
+        changeHandle = (e)=>{
+            console.log('change')
+            const {name,value} = e.target
+            // 把表单里面的值放在state中,在设置完值之后进行校验
+            this.setState({
+                [name]:value
+            },()=>{
+                // 在回调函数中进行校验
+                this.validateFile(name)
+            })
+
+        }
         // 给每个input框进行装饰
-        getFileDecorator(filed, options) {
+        getFileDecorator=(filed, option)=> {
             // 返回一个装饰器（高阶组件）
-            return function (InputComp) {
+            // 存一下options里面的数据
+            this.options[filed] = option
+            return (InputComp)=> {
                 return (
                     <div>
                         {/* 注意这里不能修改传递进来的组件，需要React.clone */}
                         {
                             React.cloneElement(InputComp, {
-                                name: filed
+                                name: filed,
+                                value:this.state[filed],
+                                onChange:this.changeHandle
                             })
+                        }
+                        {
+                            this.state[filed + "Message"]&& <p>{this.state[filed + "Message"]}</p>
                         }
                     </div>
                 )
@@ -27,7 +75,9 @@ function MyFormCreate(Comp) {
         }
         render() {
             return (
-                <Comp {...this.props} validateFile={this.validateFile}></Comp>
+                <Comp {...this.props}
+                getFileDecorator={this.getFileDecorator}
+                 validateFiles={this.validateFiles}></Comp>
             )
 
         }
@@ -47,7 +97,14 @@ class MyForm extends Component {
             <div>
                 {
                     getFileDecorator('name', {
-                        rules: [{ required: true }]
+                        rules: [{ required: true,message:"请输入name" }]
+                    })(
+                        <Input></Input>
+                    )
+                }
+                {
+                    getFileDecorator('password', {
+                        rules: [{ required: true ,message:"请输入密码"}]
                     })(
                         <Input></Input>
                     )
